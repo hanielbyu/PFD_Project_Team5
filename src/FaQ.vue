bf73a2df740ccdef8c69c38d30ded1588d3ab92a
TanTianxuJaden <s10242805@connect.np.edu.sg>
2023-11-08 19:07:50 +0800

    Created FaQ vue

---
+++ src/FaQ.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="faq-container">
+      <!-- Add your FAQ content here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "FAQ",
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your custom styles for the FAQ page here */
+  </style>
+  
+  
