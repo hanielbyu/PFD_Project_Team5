96bc9628837b34a687127c1df48e5fe5e006500b
TanTianxuJaden <s10242805@connect.np.edu.sg>
2023-11-15 20:46:43 +0800

    Push

--- src/router.js
+++ src/router.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from './components/LoginView1.vue'; 
 import TechView from './components/TechView.vue';  
 import FAQ from './components/FAQ.vue';
+import ScheduleAppointment from './components/ScheduleAppointment.vue';
+import SupportLine from './components/SupportLine.vue';
 
 const routes = [
   { path: '/', component: LoginView },
-  { path: '/tech', name: 'Tech', component: TechView },
   { path: '/faq', name: 'FAQ', component: FAQ },
+  { path: '/tech', name: 'Tech', component: TechView },
+  { path: '/ScheduleAppointment', name: 'Appointment', component: ScheduleAppointment },
+  { path: '/SupportLine', name: 'Support', component: SupportLine },
 ];
 
 const router = createRouter({
