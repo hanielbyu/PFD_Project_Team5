f81597c82277358a5566c0559e6ebfcbfced9ccf
mengteck-liew <s10247866@connect.np.edu.sg>
2023-11-16 13:08:52 +0800

    Updated the appointment page

--- src/components/ScheduleAppointment.vue
+++ src/components/ScheduleAppointment.vue
@@ -1,165 +1,159 @@
-<template>
-  <div class="appointment-container">
-    <header>Appointment Booking</header>
-    <br />
-    <a-space direction="vertical" :size="12">
-      <a-date-picker allowClear="true" v-model:value="dateselected" :disabled-date="disabledDate" />
-    </a-space>
-    <br />
-    <br />
-    <div>Time Slots Available</div>
-    <br />
-    <div class="content">
-      <div class="column">
-        <a-space wrap>
-          <a-button
-            v-for="(slot, index) in timeSlots"
-            :key="index"
-            type="primary"
-            class="button"
-            :style="{ backgroundColor: isSelected(slot) ? '#ff4d4f' : '#fff', color: isSelected(slot) ? '#fff' : '#000' }"
-            @click="selectTime(slot)"
-          >
-            {{ slot }}
-          </a-button>
-        </a-space>
-      </div>
+  <template>
+    <div class="appointment-container">
+      <header>Appointment Booking</header>
+      <br />
+      <a-space direction="vertical" :size="12">
+        <a-date-picker allowClear="true" v-model:value="dateselected" :disabled-date="disabledDate" />
+      </a-space>
+      <br />
+      <br />
+      <div>Time Slots Available</div>
+      <br />
+      <div class="content">
+        <div class="column">
+          <a-space wrap>
+            <a-button
+              v-for="(slot, index) in timeSlots"
+              :key="index"
+              type="primary"
+              class="button"
+              :style="{ backgroundColor: isSelected(slot) ? '#ff4d4f' : '#fff', color: isSelected(slot) ? '#fff' : '#000' }"
+              @click="selectTime(slot)"
+            >
+              {{ slot }}
+            </a-button>
+          </a-space>
+        </div>
+      </div><br>
+      <p>Your issue</p>
+      <a-textarea :rows="3" placeholder="type here" :maxlength="4" />
+      <br />
+      <a-checkbox v-model:checked="checked">I understand that after scheduling an appointment, I will no longer remain in the current queue.</a-checkbox>
+      <br />
+      <br />
+      <a-space wrap>
+        <a-button @click="submitAppointment" :disabled="!selectedTime || !dateselected || !checked" class="confirm-button">Confirm</a-button>
+      </a-space>
     </div>
-    <br />
-    <a-checkbox v-model:checked="checked">I understand that after scheduling an appointment, I will no longer remain in the current queue.</a-checkbox>
-    <br />
-    <br />
-    <a-space wrap>
-      <a-button @click="submitAppointment" :disabled="!selectedTime || !dateselected" class="confirm-button">Confirm</a-button>
-    </a-space>
-  </div>
-</template>
+  </template>
 
-<script>
-import { Modal } from 'ant-design-vue';
-import { h } from 'vue';
+  <script>
+  import { Modal } from 'ant-design-vue';
+  import { h } from 'vue';
 
-export default {
-  name: 'ScheduleAppointment',
-  data() {
-    return {
-      dateselected: null,
-      timeSlots: [],
-      selectedTime: null,
-      checked: false,
-    };
-  },
-  mounted() {
-    this.updateTimeSlots();
-  },
-  methods: {
-        updateTimeSlots() {
-          const now = new Date();
-          now.setMinutes(Math.ceil(now.getMinutes() / 30) * 30); 
-          const options = { hour: 'numeric', minute: 'numeric', hour12: true };
-          // for loop to generate 30 min interval buttons
-          for (let i = 0; i < 10; i++) {
-            this.timeSlots.push(now.toLocaleTimeString(undefined, options));
-            now.setMinutes(now.getMinutes() + 30);
-          }
-        },
-        selectTime(slot) {
-          console.log('Selected time:', slot);
-          this.selectedTime = slot;
-        },
-        selectdate() {
-          console.log('Selected Date:', this.dateselected);
-        },
-        isSelected(slot) {
-          return this.selectedTime === slot;
-        },
-        disabledDate(current) {
-          // Disable dates before today
-          return current && current < new Date().setHours(0, 0, 0, 0);
-        },
-        submitAppointment() {
-          if (this.selectedTime) {
-            console.log('Appointment submitted for time:', this.selectedTime);
-            this.showModal(); 
-          } else {
-            console.log('Please select a time slot before submitting.');
-          }
-        },
-    showModal() {
-      Modal.confirm({
-        title: 'Schedule Appointment',
-        content: h('div', {}, [
-          h('p', `Selected Date: ${this.dateselected ? this.dateselected.format('YYYY-MM-DD') : 'Not selected'}`),
-          h('p', `Selected Time: ${this.selectedTime || 'Not selected'}`),
-        ]),
-        onOk: () => {
-          if (this.selectedTime && this.dateselected) {
-            console.log('OK');
-          } else {
-            console.log('Error: Please select a time slot.');
-            return false;
-          }
-        },
-        onCancel() {
-          console.log('Cancel');
-        },
-        class: 'appointment-modal',
-      });
+  export default {
+    name: 'ScheduleAppointment',
+    data() {
+      return {
+        dateselected: null,
+        timeSlots: [],
+        selectedTime: null,
+        checked: false,
+      };
     },
-  },
-};
-</script>
+    mounted() {
+      this.updateTimeSlots();
+    },
+    methods: {
+          updateTimeSlots() {
+            const now = new Date();
+            now.setMinutes(Math.ceil(now.getMinutes() / 30) * 30); 
+            const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+            // for loop to generate 30 min interval buttons
+            for (let i = 0; i < 10; i++) {
+              this.timeSlots.push(now.toLocaleTimeString(undefined, options));
+              now.setMinutes(now.getMinutes() + 30);
+            }
+          },
+          selectTime(slot) {
+            console.log('Selected time:', slot);
+            this.selectedTime = slot;
+          },
+          selectdate() {
+            console.log('Selected Date:', this.dateselected);
+          },
+          isSelected(slot) {
+            return this.selectedTime === slot;
+          },
+          disabledDate(current) {
+            // Disable dates before today
+            return current && current < new Date().setHours(0, 0, 0, 0);
+          },
+          submitAppointment() {
+            if (this.selectedTime) {
+              console.log('Appointment submitted for time:', this.selectedTime);
+              this.showModal(); 
+            } else {
+              console.log('Please select a time slot before submitting.');
+            }
+          },
+      showModal() {
+        Modal.confirm({
+          title: 'Schedule Appointment',
+          content: h('div', {}, [
+            h('p', `Selected Date: ${this.dateselected ? this.dateselected.format('YYYY-MM-DD') : 'Not selected'}`),
+            h('p', `Selected Time: ${this.selectedTime || 'Not selected'}`),
+          ]),
+          onOk: () => {
+            if (this.selectedTime && this.dateselected) {
+              console.log('OK');
+            } else {
+              console.log('Error: Please select a time slot.');
+              return false;
+            }
+          },
+          onCancel() {
+            console.log('Cancel');
+          },
+          class: 'appointment-modal',
+        });
+      },
+    },
+  };
+  </script>
 
-<style scoped>
-.appointment-container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 10px;
-}
+  <style scoped>
+  .appointment-container {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+  }
+  .content {
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.content {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-}
+  .column {
+    width: 100%; /* Set the initial width to 100% for one column */
+    box-sizing: border-box;
+  }
 
-.column {
-  flex: 0.5;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-}
+  .button {
+    width: 80px;
+    margin: 4px; /* Add some margin between buttons for spacing */
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+  }
+  .button:hover {
+    background-color: #fff;
+    color: #000;
+  }
 
-.button {
-  width: 100%;
-  height: 50px;
-  text-align: center;
-  border: 1px solid #ccc;
-}
+  .confirm-button {
+    background-color: #ff4d4f;
+    color: #000;
+  }
 
-.button:hover {
-  background-color: #fff;
-  color: #000;
-}
+  .confirm-button:hover {
+    background-color: #fff;
+    color: #000;
+  }
 
-.confirm-button {
-  background-color: #ff4d4f;
-  color: #000;
-}
-
-.confirm-button:hover {
-  background-color: #fff;
-  color: #000;
-}
-
-button:disabled {
-  background-color: #ccc;
-  color: #fff;
-  cursor: not-allowed;
-}
-</style>
+  button:disabled {
+    background-color: #ccc;
+    color: #fff;
+    cursor: not-allowed;
+  }
+  </style>
